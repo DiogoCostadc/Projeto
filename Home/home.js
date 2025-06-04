1a128281e3211b4247c0df23a51a80a3d949b4f2
AndreRibeiroESMAD <40240475@esmad.ipp.pt>
2025-06-04 16:49:59 +0100

    dnghdsj

--- Home/home.js
+++ Home/home.js
@@ -7,93 +7,143 @@ let uid = token.data.user.id
 console.log(uid)
 
 let createplan = document.getElementById("create")
-let col1 = document.getElementById("col-1")
-let col2 = document.getElementById("col-2")
+let page = 1
+let body = document.getElementById("screen-body")
+document.querySelectorAll('#change').forEach(link => {
+    link.addEventListener('click', function(event) {
+        pagechange();
+    });
+});
 
-let user = await supabase        //buscar user
+CurrentPage()
+
+function CurrentPage(){
+    if (page == 1){
+        body.innerHTML = `<div class="screen-space" id="main-body">
+                        <h2 class="text-maincolor" style="text-align: center;">What's new?</h1>
+                        <div class="news-board"> <!-- What's new board -->
+                            <label>A melhor app entre a turma toda 100% confiavel</label>
+                        </div>
+                        <hr>
+                        <div class="list"> <!-- Lista -->
+                            <div id="col-1" class="cols"> <!-- Coluna 1 -->
+                            </div>
+                            <div id="col-2" class="cols"> <!-- Coluna 2 -->
+                            </div>
+                        </div>
+                    </div>`
+    }
+    else{
+        body.innerHTML = `<div class="screen-space" id="main-body">
+                        <div class="list"> <!-- Lista -->
+                            <div id="col-1" class="cols"> <!-- Coluna 1 -->
+                            </div>
+                            <div id="col-2" class="cols"> <!-- Coluna 2 -->
+                            </div>
+                        </div>
+                    </div>`
+    }
+    Render()
+}
+
+async function Render() {
+    let col1 = document.getElementById("col-1")
+    let col2 = document.getElementById("col-2")
+    let user = await supabase        //buscar user
     .from('User')
     .select()
     .eq("user_id", uid)
     let userfinal = user.data[0].id
 
-let planos = await supabase
-    .from("User-Planos")            //procura planos com o id do user
-    .select()
-    .eq("user", userfinal)
-
-let array = []                  //id dos planos num array
-let planosid = planos.data
-
-console.log(planos.data)
-
-planosid.forEach(planosid => {
-    console.log(planosid.planos)
-
-    array.push(planosid.planos)       //mete os ids no array
-    console.log(array)
-});
-
-let planoinfo = await supabase
-    .from("Planos")                     //procura info de cada plano do array
-    .select()
-    .in('id', array)
-console.log(planoinfo.data)
-
-let array2 = planoinfo.data
-
-
-for (let i = 0; i < array2.length; i++) {       //adiciona os planos num butão
-    let plan = array2[i];
-    console.log(plan.nome);
-    console.log(plan);
-
-    let btn = document.createElement("button");
-    btn.id = plan.id;
-    btn.textContent = plan.nome;
-
-    let planlayout = `<div class="plan">
-                <div class="plan-gui">
-                    <label>
-                        ${plan.nome}
-                    </label>
-                </div>
-            </div>`
-    if ((i % 2) == 0 ){
-        col1.innerHTML += planlayout
-    }
-    else{
-        col2.innerHTML += planlayout
-    }
-
-    btn.addEventListener("click", function(event) {        
-        let btnid = event.target.id
-        console.log(btnid)                  
-        teste()
-        async function teste(){
-            let planoteste = await supabase     //procura na planos-exercicios por exercicios no id do plano
-            .from("Plano-Exercicio")
-            .select()
-            .eq("plano", btnid)
-        console.log(planoteste.data)
-        
-        let exerarray = []              //guarda o id dos exercicios do plano
-
-        planoteste.data.forEach(exercicio => {
-            console.log(exercicio.exercicio)
-            exerarray.push(exercicio.exercicio)
-        });
-        console.log(exerarray)
-
-        let exercicioinfo = await supabase      //procura info do exercicio
-        .from("Exercicio")
+    let planos = await supabase
+        .from("User-Planos")            //procura planos com o id do user
         .select()
-        .in("id", exerarray)
-        console.log(exercicioinfo.data)
+        .eq("user", userfinal)
 
-        
+    let array = []                  //id dos planos num array
+    let planosid = planos.data
+
+    console.log(planos.data)
+
+    planosid.forEach(planosid => {
+        console.log(planosid.planos)
+
+        array.push(planosid.planos)       //mete os ids no array
+        console.log(array)
+    });
+
+    let planoinfo = await supabase
+        .from("Planos")                     //procura info de cada plano do array
+        .select()
+        .in('id', array)
+    console.log(planoinfo.data)
+
+    let array2 = planoinfo.data
+
+
+    for (let i = 0; i < array2.length; i++) {       //adiciona os planos num butão
+        let plan = array2[i];
+        console.log(plan.nome);
+        console.log(plan);
+
+        let btn = document.createElement("button");
+        btn.id = plan.id;
+        btn.textContent = plan.nome;
+
+        let planlayout = `<a href="#">
+                            <div class="plan">
+                                <div class="plan-gui">
+                                    <label>
+                                        ${plan.nome}
+                                    </label>
+                                </div>
+                            </div>
+                        </a>`
+        if ((i % 2) == 0 ){
+            col1.innerHTML += planlayout
         }
-    }) 
+        else{
+            col2.innerHTML += planlayout
+        }
+
+        btn.addEventListener("click", function(event) {        
+            let btnid = event.target.id
+            console.log(btnid)                  
+            teste()
+            async function teste(){
+                let planoteste = await supabase     //procura na planos-exercicios por exercicios no id do plano
+                .from("Plano-Exercicio")
+                .select()
+                .eq("plano", btnid)
+            console.log(planoteste.data)
+            
+            let exerarray = []              //guarda o id dos exercicios do plano
+
+            planoteste.data.forEach(exercicio => {
+                console.log(exercicio.exercicio)
+                exerarray.push(exercicio.exercicio)
+            });
+            console.log(exerarray)
+
+            let exercicioinfo = await supabase      //procura info do exercicio
+            .from("Exercicio")
+            .select()
+            .in("id", exerarray)
+            console.log(exercicioinfo.data)
+
+            
+            }
+        }) 
+    }
 }
+
+function pagechange(){
+    page = page === 1 ? 2 : 1;
+    CurrentPage()
+    console.log("triggered")
+}
+
+
 
 /*
 array2.forEach(planos => {              //cria butao como nome de cada array
@@ -112,7 +162,6 @@ array2.forEach(planos => {              //cria butao como nome de cada array
 // async function teste() {
     //  console.log(event.srcElement.id)
 //}
-
 
 createplan.addEventListener("click", async()=>{
 
