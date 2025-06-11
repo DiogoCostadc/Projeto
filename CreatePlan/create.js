c2f48c59e9fbc4798559c8a58b1195fd91f8e00e
DiogoCostadc <40240308@esmad.ipp.pt>
2025-06-11 15:56:02 +0100

    update ao js
    organisar o codigo

--- CreatePlan/create.js
+++ CreatePlan/create.js
@@ -2,15 +2,21 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient('https://lbagxsindniuqrinfyug.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiYWd4c2luZG5pdXFyaW5meXVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5MTg3NDUsImV4cCI6MjA2MzQ5NDc0NX0.vJ4oWcxhWsvytyRGqaKowYPPQaU1hvMLOSSLPU0T4AU')
 
+//Variaveis HTML
 //------------------------------------------------------
 let addbut = document.getElementById("addbut")
-let form = document.getElementById("testform")
 let exe = document.getElementById("exer")
 let exlist = document.getElementById("exlist")
 let planid = localStorage.getItem("CreateIdPlan")
 let arrayexercicio = []
 let but = document.getElementById("but")
 let cancelbut = document.getElementById("cancel")
+let exinfoname 
+let exinfodesc 
+let exinfoobs
+//------------------------------------------------------
+
+//Informação do utilizador como Uid, ID e ID do plano criado
 //------------------------------------------------------
 let token = await supabase.auth.getUser()
 let uid = token.data.user.id
@@ -25,47 +31,47 @@ let user = await supabase        //buscar user
 
 console.log(userfinal)
 //----------------------------------------------------
+
+//criação de array com info de todos os exercicios e um array com o id de cada exercicio
+//----------------------------------------------------
 let exercicio = await supabase
     .from('Exercicio')
     .select()
     console.log(exercicio.data)
     let exercicios = exercicio.data
-    let exerarray = []
-    exercicios.forEach(teste => {
-        exerarray.push(teste.id)
-       
-    });
-    console.log(exerarray)
+    
 
 //------------------------------------------------------------------------
 
+//Butão para criar a lista de exercicios para o utilizador escolher
+//------------------------------------------------------------------------
 addbut.addEventListener("click", async()=>{
-    
-
-
-for (let i = 0 ; i < exerarray.length ; i++){
-    let teste = await supabase
-    .from("Exercicio")
-    .select()
-    .eq ("id", exerarray[i] )
-    console.log(teste.data[0].nome)
-
-  let opt = document.createElement("option")
-  opt.id = teste.data[0].id
-  opt.textContent = teste.data[0].nome
-  opt.value = teste.data[0].id
   
 
+//for para adicionar os planos a um select no html
+//-------------------------------------------------
+for (let i = 0 ; i < exercicios.length ; i++){ 
+console.log(exercicios[i].nome)
+
+  let opt = document.createElement("option")
+  opt.id = exercicios[i].id
+  opt.textContent = exercicios[i].nome
+  opt.value = exercicios[i].id
   exe.appendChild(opt)
 }
+//---------------------------------------------------
+
+//Sempre que houver mudança no select 
+//-------------------------------------------------------
+//Adiciona exercicio escolhido a um array
  exe.addEventListener("change", function(event){
     let optid = event.target.value
     console.log(optid)
   arrayexercicio.push(optid)
-    let exinfoname 
-    let exinfodesc 
-    let exinfoobs
 console.log(arrayexercicio)
+
+//Procura info de exercicio escolhido e adiciona ao html 
+//-------------------------------------------------------
     teste()
    async function teste() {
         let ex = await supabase
@@ -77,9 +83,7 @@ console.log(arrayexercicio)
         exinfoname = ex.data[0].nome
         exinfodesc = ex.data[0].desc
         exinfoobs = ex.data[0].obs
-
-        
-        
+ 
       exlist.innerHTML += `<div class="lista-item">
                             <div>
                                 <div class="info">
@@ -95,37 +99,47 @@ console.log(arrayexercicio)
                                 <img src="https://firstbenefits.org/wp-content/uploads/2017/10/placeholder-300x300.png" alt="placeholder" />
                             </div>
                         </div>`
-
-      //`<p>nome: ${exinfoname}, desc : ${exinfodesc}, obs : ${exinfoobs}</p>`
    }
-  
+   //-------------------Fim da procura de info do plano------------------------------------------------------
   })
-
-
+  //--------------------Fim da função do select-------------------------------------------------------
 })
+//----------------------Fim da função principal que adiciona planos ao select-----------------------------------------------------
 
+//Butão para confimar e adicionar os exercicios selecionados ao plano
+//-------------------------------------------------------------------------------------------------
 but.addEventListener("click", async()=>{
 
   console.log(arrayexercicio)
-  let arraytest = []
+  let addPlanArray = []
   arrayexercicio.forEach(exerID => {
-    arraytest.push({plano: planid, exercicio : exerID})
+    addPlanArray.push({plano: planid, exercicio : exerID})
   });
-  console.log(arraytest)
+  console.log(addPlanArray)
   let insert = await supabase
   .from('Plano-Exercicio')
-  .insert(arraytest)
+  .insert(addPlanArray)
 
   alert("Plano criado")
-  window.location.replace("../Home/index.html")
+  homepage()
 })
+//------------------------------------------------------------------------------------------------------
 
+//Butão para cancelar a apagar o plano que está a ser criado
+//------------------------------------------------------------------------------------------------------
 cancelbut.addEventListener("click", async()=>{
   let deleteplan = await supabase
   .from('Planos')
   .delete()
   .eq('id', planid )
 
-  window.location.replace("../Home/index.html")
+  homepage()
 })
+//-------------------------------------------------------------------------------------------------------
+
+//Função para voltar para a homepage
+//-------------------------------------------------------------------------------------------------------
+function homepage(){
+window.location.replace("../Home/index.html")
+}
 
--- Myplan/myplan.js
+++ Myplan/myplan.js
@@ -2,47 +2,52 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient('https://lbagxsindniuqrinfyug.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiYWd4c2luZG5pdXFyaW5meXVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5MTg3NDUsImV4cCI6MjA2MzQ5NDc0NX0.vJ4oWcxhWsvytyRGqaKowYPPQaU1hvMLOSSLPU0T4AU')
 
-let currentPlan = localStorage.getItem("currentPlan")
-let header = document.getElementById("header")
+//Variaveis do HTML
+//-----------------------------------------------------------------------
+let currentPlan = localStorage.getItem("currentPlan") //id do plano atual
+let header = document.getElementById("header")      
 let body = document.getElementById("list")
 let share = document.getElementById("share")
 let backbut = document.getElementById("backbut")
 let deleteplan = document.getElementById("deletebut")
 console.log(currentPlan)
-
+//------------------------------------------------------------------------
+//Nome do plano
 let plano = await supabase
 .from("Planos")
 .select()
 .eq("id", currentPlan)
 let planName = plano.data[0].nome
+//-----------------------------------------------
 
-
-
+//Array com relação exercicio-plano
 let exercicio = await supabase
 .from("Plano-Exercicio")
 .select()
 .eq("plano", currentPlan)
-
 console.log(exercicio.data)
+//------------------------------------------
 
-let exerarray = []              //guarda o id dos exercicios do plano
-
+//exerarray guarda os ids dos exercicios 
+    let exerarray = []             
+    //-------------------------------------
     exercicio.data.forEach(exercicio => {
         console.log(exercicio.exercicio)
         exerarray.push(exercicio.exercicio)
     });
     console.log(exerarray)
+    //-------------------------------------
 
-    let exercicioinfo = await supabase      //procura info do exercicio
+    let exercicioinfo = await supabase //procura info do exercicio
     .from("Exercicio")
     .select()
     .in("id", exerarray)
-    console.log(exercicioinfo.data)
+    console.log(exercicioinfo.data) //array com informação de todos os exercicios do plano
     let exinfo = exercicioinfo.data
-
+//-------------------------------------------------------------
     
-    
-
+//Função para mostrar os exercicios
+//------------------------------------------------------------------
 planinfo()
 async function planinfo() {
     header.innerHTML = `${planName}`
@@ -51,16 +56,12 @@ let exercicioinfo = await supabase      //procura info do exercicio
     .select()
     .in("id", exerarray)
 
+//For cria e adiciona html com a informação de cada plano
+//--------------------------------------------------------------------
 for (let i = 0 ; i < exinfo.length; i++){
      
     console.log(exercicioinfo.data[i])
     let info = exercicioinfo.data[i]
-   
-
-
-    
-
-    //` <p>nome : ${info.nome}, descrição : ${info.desc}, observação : ${info.obs}</p>` 
 
     body.innerHTML += `<div class="lista-item">
                         <div class="info">
@@ -75,11 +76,11 @@ for (let i = 0 ; i < exinfo.length; i++){
                         </div>
                     </div>`
 }
-
-
-
+//--------------------------------------------------------------
 }
 
+//Butão de partilha para adicionar plano a uma tabela com todos os planos partilhados por utilizadores
+//--------------------------------------------------------------
 share.addEventListener("click", async()=>{
     let {data, error} = await supabase
     .from("Partilhados")
@@ -97,11 +98,17 @@ share.addEventListener("click", async()=>{
     alert("Plano Partilhado")
     }
 })
+//------------------------------------------------------------
 
+//Butão para voltar para o menu principal
+//--------------------------------------------------------------
 backbut.addEventListener("click", async()=>{
-    window.location.replace("../Home/index.html")
+    homepage()
 })
+//----------------------------------------------
 
+//Butão para apagar o plano
+//-----------------------------------------------
 deleteplan.addEventListener("click", async()=>{
     let deleteplan = await supabase
   .from('Planos')
@@ -109,7 +116,13 @@ deleteplan.addEventListener("click", async()=>{
   .eq('id', currentPlan )
 
  console.log(deleteplan) 
- 
-
-  window.location.replace("../Home/index.html")
+ homepage()
 })
+//------------------------------------------------
+
+//Função para voltar ao menu principal
+//------------------------------------------------
+function homepage(){
+window.location.replace("../Home/index.html")
+}
+//-------------------------------------------------
