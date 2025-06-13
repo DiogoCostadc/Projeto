6ca576180d510e948352606c8d4063fbe4804ebd
DiogoCostadc <40240308@esmad.ipp.pt>
2025-06-13 12:23:13 +0100

    update
    adicionado sw manifest

--- login.js
+++ login.js
@@ -2,6 +2,12 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient('https://lbagxsindniuqrinfyug.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiYWd4c2luZG5pdXFyaW5meXVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5MTg3NDUsImV4cCI6MjA2MzQ5NDc0NX0.vJ4oWcxhWsvytyRGqaKowYPPQaU1hvMLOSSLPU0T4AU')
 
+if("serviceWorker" in navigator){
+    navigator.serviceWorker.register("./sw.js").then(function(){
+        console.log("Service Worker is registered")
+    })
+  }
+
 let screen = document.getElementById("frame")
 let LogType = "login"
 let Usertoken = null
---
+++ sw.js
@@ -0,0 +1,85 @@
+let cache_static_1 = "Static-v1"
+
+let cache_dynamic_1 = "Dynamic-v1"
+
+let offline_url = "offline.html"
+
+let offlinestyle = "login.css"
+
+let homeHtml = "./Home/index.html"
+
+let homeJS = "./Home/home.js"
+
+let homeStyle = "./Home/style.css"
+
+let myplanHTML = "./Myplan/myplan.html"
+
+let myPlanJS = "./Myplan/myplan.js"
+
+let myplayStyle = "./Myplan/style.css"
+
+self.addEventListener("install", function(event){
+    event.waitUntil(
+        caches.open(cache_static_1)
+        .then(function(cache){
+           return cache.addAll([offline_url, offlinestyle])
+        })
+    )
+})
+
+self.addEventListener("activate", function(event){
+    event.waitUntil(
+        caches.keys()
+        .then(function(keyList){
+        return Promise.all(keyList.map(function(key){
+            if (key !== cache_static_1){
+                return caches.delete(key)
+            }
+        }))
+        })
+    )
+})
+
+self.addEventListener("fetch", function(event){
+    if(event.request.mode === "navigate"){
+        event.respondWith(
+            (async () => {
+                try{
+                    const networkResponse = await fetch(event.request);
+                    return networkResponse
+                } catch (error){
+                    console.log("Fetch failed returing to offline page", error);
+                    
+                    const cache = await caches.open(cache_static_1);
+                    const cachedResponse = await cache.match(offline_url);
+                    return cachedResponse
+                }
+            })()
+        )
+    }else{
+        
+            event.respondWith(
+                caches.match(event.request)
+                .then(function(response){
+                    if (response){
+                        return response
+                    } else {
+                        return fetch(event.request)
+                        .then (function(res){
+                            return caches.open(cache_dynamic_1)
+                            .then(function(cache){
+                                cache.put(event.request.url, res.clone())
+                                return res
+                            })
+                        })
+                        .catch( async function(err){
+                          let cache = await caches.open(cache_static_1)
+                          let fallbackResponse = await cache.match(event.request)
+                          return fallbackResponse
+                        })
+                    }
+                })
+            )
+    
+    }
+})
